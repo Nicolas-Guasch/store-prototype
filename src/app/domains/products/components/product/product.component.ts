0233a05b0fda923e96a9466aa59088670d27aa1f
Nicolas-Guasch <nic.guasch@gmail.com>
2024-09-23 17:46:43 -0300

    Product component rendered

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent {
-
+  img = 'https://picsum.photos/640/640?r=' + Math.random();
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from '../../components/product/product.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
-export class ListComponent {
-
-}
+export class ListComponent {}
