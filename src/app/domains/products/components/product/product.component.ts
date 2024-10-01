fe63e95a8d83ca2378d3e16f624e1949af4699c1
Nicolas-Guasch <nic.guasch@gmail.com>
2024-09-30 21:44:23 -0300

    Make product page reactive

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, input, output } from '@angular/core';
 
+import { Product } from '../../../shared/components/counter/models/product.model';
+
 @Component({
   selector: 'app-product',
   standalone: true,
@@ -8,14 +10,16 @@ import { Component, EventEmitter, input, output } from '@angular/core';
   styleUrl: './product.component.css',
 })
 export class ProductComponent {
-  img = input.required<string>();
-  price = input.required<number>();
-  title = input.required<string>();
+  product = input.required<Product>();
 
   addToCart = output<string>();
 
   addToCartHandler() {
     console.log('click from child');
     this.addToCart.emit('Message from child');
+  }
+
+  parseDate(ISODate: string): string {
+    return new Date(ISODate).toDateString().split(' ').slice(1).join(' ');
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+
+import { Product } from '../../../shared/components/counter/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -9,6 +11,34 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const templateProduct: Product = {
+      id: Date.now(),
+      title: 'Product',
+      price: 100,
+      image: 'https://picsum.photos/640/640?r=',
+      publishedOn: new Date().toISOString(),
+    };
+    let test = new Date(templateProduct.publishedOn);
+    console.log(test.toDateString());
+
+    const defaultAmount = 12;
+    let defaultProducts: Product[] = [];
+    for (let i = 1; i <= defaultAmount; i++) {
+      defaultProducts.push({
+        id: templateProduct.id + i,
+        title: templateProduct.title + ' ' + i,
+        price: Math.trunc(Math.random() * 1000),
+        image: templateProduct.image + i,
+        publishedOn: new Date().toISOString(),
+      });
+    }
+
+    this.products.set(defaultProducts);
+  }
+
   fromChild(event: string) {
     console.log('Parent receiving...');
     console.log(`child sent: ${event}`);
