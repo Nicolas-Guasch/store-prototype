3fe546feed6598736b668956d1cf8b8f92098717
Nicolas Guasch <nic.guasch@gmail.com>
2024-10-09 20:03:49 -0300

    Add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
+import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,9 @@ export const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: ListComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
