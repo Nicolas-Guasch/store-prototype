1de71fbebe331639ebe87eaff3af484233cd57ec
Nicolas Guasch <nic.guasch@gmail.com>
2024-10-09 20:52:10 -0300

    Move header to separate layout for nested routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,23 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 
 export const routes: Routes = [
   {
-    path: 'about',
-    component: AboutComponent,
-  },
-  {
     path: '',
-    pathMatch: 'full',
-    component: ListComponent,
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ListComponent,
+      },
+    ],
   },
   {
     path: '**',
---
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [HeaderComponent, RouterModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css',
+})
+export class LayoutComponent {}
